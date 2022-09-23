0b3d63db94e6569e5d0ee6dee72089a6423116b3
Leandro Soares <98189939+SoaresLMB@users.noreply.github.com>
2022-09-23 14:04:55 -0300

    Add files via upload

---
+++ execucao_do_algoritmo.py
@@ -0,0 +1,38 @@
+from classe_matriz import Matriz
+
+# EXECUTANDO O ALGORITMO:
+
+# 1.1: WSM
+
+# Construindo o objeto Matriz_1 passando como parâmetro a quantidade de alternativas e de citérios.
+matriz_1 = Matriz(2,2)
+# Aplicando o metodo WSM
+matriz_1.metodo_wsm()
+
+# 1.2: WPM
+
+# Construindo o objeto Matriz_2 passando como parâmetro a quantidade de alternativas e de citérios.
+matriz_2 = Matriz(2,2)
+# Aplicando o metodo WPM
+matriz_2.metodo_wpm()
+
+# 1.3: MABAC
+
+# Construindo o objeto Matriz_3 passando como parâmetro a quantidade de alternativas e de citérios.
+matriz_3 = Matriz(2,2)
+# Aplicando o metodo MABAC
+matriz_3.metodo_mabac()
+
+# 1.4: WASPAS
+
+# Construindo o objeto Matriz_4 passando como parâmetro a quantidade de alternativas e de citérios.
+matriz_4 = Matriz(2,2)
+# Aplicando o metodo WASPAS:
+matriz_4.metodo_waspas()
+
+
+
+
+
+
+
